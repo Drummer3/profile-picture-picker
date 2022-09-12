2076ae81f6b437fc8a388c46cf603dcce3ea9da9
Data Munjishvili <data.munjishvili@pulsar.ai>
2022-09-12 17:19:17 +0400

    and drag and match badge

--- components/PhotoEditor.tsx
+++ components/PhotoEditor.tsx
@@ -1,10 +1,4 @@
-import {
-	ChangeEvent,
-	MouseEvent,
-	useEffect,
-	useRef,
-	useState,
-} from 'react'
+import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from 'react'
 import profilePhoto from '../assets/human.jpg'
 import styles from '../styles/ProfilePhoto.module.scss'
 export default function PhotoEditor() {
@@ -78,14 +72,34 @@ export default function PhotoEditor() {
 					className="hidden"
 					onChange={handleFileUpload}
 				/>
-				<canvas
-					ref={canvasRef}
-					className="relative my-4 mx-auto w-full aspect-square overflow-hidden"
-					draggable
-					onDragStart={handleDraggingStart}
-					onDragOver={handleDragging}
-				>
-					{/* <div className="absolute flex justify-center items-center inset-0 z-50 backdrop-brightness-50">
+				<div className="relative">
+					<div className="z-50 top-8 text-white absolute w-full flex items-center justify-center">
+						<p className="flex justify-center items-center text-center px-4 py-2 backdrop-brightness-50 rounded-xl">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="w-6 h-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5"
+								/>
+							</svg>
+							drag and match
+						</p>
+					</div>
+					<canvas
+						ref={canvasRef}
+						className="relative my-4 mx-auto w-full aspect-square overflow-hidden"
+						draggable
+						onDragStart={handleDraggingStart}
+						onDragOver={handleDragging}
+					>
+						{/* <div className="absolute flex justify-center items-center inset-0 z-50 backdrop-brightness-50">
 						<div className="w-[96%] h-[96%] rounded-full backdrop-brightness-200"></div>
 					</div>
 					<Image
@@ -101,7 +115,8 @@ export default function PhotoEditor() {
 						}}
 						quality={100}
 					/> */}
-				</canvas>
+					</canvas>
+				</div>
 				<div className="text-neutral-500 gap-2 flex justify-between items-center">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
